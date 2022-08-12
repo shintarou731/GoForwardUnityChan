3f7e2a8f3eab4e2fdf679cb47dc0e0ba3de50a15
shintarou731 <109932469+shintarou731@users.noreply.github.com>
2022-08-12 19:57:09 +0900

    kousin
    8/12

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class UnityChanController : MonoBehaviour
 {
+    //アニメーションをするためのコンポーネントを入れる
+    Animator animator;
+
+    //unityちゃんを移動させるコンポーネントを入れる
+    Rigidbody2D rigit2D;
+
+    //地面の位置
+    private float groundLevel = -3.0f;
+
+    //ジャンプの速度の減衰
+    private float dump = 0.8f;
+
+    //ジャンプの速度
+    float jumpVelocity = 20;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //アニメータのコンポーネントを取得
+        this.animator = GetComponent<Animator>();
+        //Rigitbody2Dのコンポーネントを取得する
+        this.rigit2D = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //走るアニメーションを再生するために、Animatorのパラメータを調節する
+        this.animator.SetFloat("Horizontal", 1);
+
+        //着地しているかどうかを調べる
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+
+        //着地状態でクリックされた場合
+        if(Input.GetMouseButtonDown(0) && isGround)
+        {
+            //上方向の力をかける
+            this.rigit2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+
+        //クリックをやめたら上方向への速度を減衰する
+        if(Input.GetMouseButton(0) == false)
+        {
+            if(this.rigit2D.velocity.y > 0)
+            {
+                this.rigit2D.velocity *= this.dump;
+            }
+        }
     }
 }
